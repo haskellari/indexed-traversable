6ca3ad88b679b3e92203cec89d639b7215f4702f
Oleg Grenrus <oleg.grenrus@iki.fi>
2020-12-27 14:25:36 +0200

    Mark all modules explicitly as Safe
    Add an internal module marking `build` as Trustworthy
    https://gitlab.haskell.org/ghc/ghc/-/issues/19127

--- indexed-traversable/src/Data/Traversable/WithIndex.hs
+++ indexed-traversable/src/Data/Traversable/WithIndex.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE CPP         #-}
+#if __GLASGOW_HASKELL__ >= 704
+{-# LANGUAGE Safe        #-}
+#elif __GLASGOW_HASKELL__ >= 702
+{-# LANGUAGE Trustworthy #-}
+#endif
 -- | Indexed Traversables
 module Data.Traversable.WithIndex (
     -- * Indexed Traversables
